6610a8d912a7c8d77e0f1549dd47b64be8971f71
Le Tuan Dat <dattuanle07cs@gmail.com>
2013-07-18 21:35:05 +0700

    gem rails_12factor group: :production

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,9 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+if defined?(Bundler)
+  Bundler.require(:default, :assets, Rails.env)
+end
 
 module Getstart
   class Application < Rails::Application
