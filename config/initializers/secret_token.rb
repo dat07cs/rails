ebaae89d7b8d8f3a0651709334ff080019efc246
Le Tuan Dat <dattuanle07cs@gmail.com>
2013-07-18 18:36:40 +0700

    first getstart commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getstart::Application.config.secret_key_base = '3b94c17b39e947ac5c82531ae0f8a0e850f79ed4f920dbde389ac0eadb0ec216c7be39323a0a7fbe9358b1efcaf0cc2d3b6df031ece7580d69c6eb787922f5df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Getstart::Application.config.session_store :cookie_store, key: '_getstart_session'
